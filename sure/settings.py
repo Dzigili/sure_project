2086f157c098dba1f3c2f8e0e560ee7817a943d8
Igor Vujkovic <igor.vujkovic@gmail.com>
2018-10-26 12:34:10 +0200

    connected with a postgresql

--- sure/settings.py
+++ sure/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'sure.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'suredb',
+        'USER': 'sure',
+        'PASSWORD': '123',
+        'HOST': 'localhost'
     }
 }
 
