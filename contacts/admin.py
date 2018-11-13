b803216c61aa3cc43717a155fced92738834d6f0
Igor Vujkovic <igor.vujkovic@gmail.com>
2018-11-13 18:55:26 +0100

    Contact Form

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contacts
 
-# Register your models here.
+class ContactsAdimn(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+admin.site.register(Contacts, ContactsAdimn)
